4fa8a9caa3edd6ef3242de654121f196b05969a8
Toby Lazar <tlazar@capitaltg.com>
2019-06-13 01:06:30 -0400

    initial commit

---
+++ src/main/java/com/capitaltg/kafka/KafkaConfiguration.java
@@ -0,0 +1,48 @@
+package com.capitaltg.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@EnableKafka
+@Configuration
+public class KafkaConfiguration {
+
+  @Value("${kafka.group.id:the-group}")
+  private String groupId;
+  @Value("${kafka.bootstrap.servers}")
+  private String bootstrapServers;
+
+  /**
+   * Creates default KafkaConsumerFactory.
+   */
+  @Bean
+  public ConsumerFactory<String, String> consumerFactory() {
+    Map<String, Object> config = new HashMap<>();
+    config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    return new DefaultKafkaConsumerFactory<>(config);
+  }
+
+  /**
+   * Creates KafkaListener Container Factory.
+   */
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, String> factory
+        = new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(consumerFactory());
+    return factory;
+  }
+}
